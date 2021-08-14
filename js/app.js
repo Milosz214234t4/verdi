8fee98ea347b9a79c5fb7476b4cf4338b95579b5
Milosz214234t4 <78423365+Milosz214234t4@users.noreply.github.com>
2021-08-14 15:39:22 +0200

    dzialajacy hamburger

--- js/app.js
+++ js/app.js
@@ -1,9 +1,30 @@
-// const ham = document.querySelector('.hamburger');
-// const nav = document.querySelector('.navigation');
 
-// const handleClick = () => {
-  // hamburger.classList.toggle('hamburger--active');
-  // nav.classList.toggle('navigation--active');
-// }
 
-// hamburger.addEventListener('click', handleClick);
+function hamburger(){
+  const mediaQuery = window.matchMedia('(max-width: 800px)');
+  console.log(mediaQuery);
+  let x = document.querySelector("nav");
+  console.log(x);
+  x.classList.toggle("navigation-active");
+ let y = document.querySelector(".hamburger");
+ console.log(y);
+ y.classList.toggle("hamburger--active");
+ if(y.classList.contains("hamburger--active")){
+   let z = document.querySelectorAll("nav ul li a");
+   console.log(z);
+   let li = document.querySelectorAll("nav ul li");
+   for(let x = 0; x < li.length; x++){
+     li[x].classList.add("margin")
+   }
+   let ul = document.querySelector("nav ul");
+   console.log(ul);
+   ul.classList.add("no-pointer");
+for(let x = 0; x < z.length; x++){
+  z[x].classList.add("list-style");
+}
+
+ }
+ 
+
+}
+
