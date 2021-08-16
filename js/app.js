e18cf0c8de9c27f302bd1d8ffcc1aa19120f723c
Milosz214234t4 <78423365+Milosz214234t4@users.noreply.github.com>
2021-08-16 17:37:21 +0200

    hamburger not yet

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
 
 function hamburger(){
   const mediaQuery = window.matchMedia('(max-width: 800px)');
-  console.log(mediaQuery);
   let x = document.querySelector("nav");
   console.log(x);
   x.classList.toggle("navigation-active");
@@ -28,3 +27,44 @@ for(let x = 0; x < z.length; x++){
 
 }
 
+function dropdown()
+{let mediaQuery = window.matchMedia('(max-width: 800px)');
+
+let nav = document.querySelector("nav");
+console.log(mediaQuery.matches);
+// console.log(nav.classList.contains("navigation-active"))
+ if(!mediaQuery.matches){
+  let x = document.querySelector("#dropdown-div");
+  // console.log(x);
+  x.classList.add("dropdown-hover");
+  // x.classList.contains("dropdown-content")
+
+    x.classList.remove("dropdown-content");
+  
+  let y = document.querySelector(".no-pointer");
+  y.classList.add("grid");
+ }
+
+ 
+}
+
+function dropdownnone(){
+  
+  let mediaQuery = window.matchMedia('(max-width: 800px)');
+  // console.log(mediaQuery.matches);
+    // console.log("jestem w petli")
+    if(!mediaQuery.matches){
+  let x = document.querySelector("#dropdown-div");
+  // console.log(x);
+
+  x.classList.remove("dropdown-hover");
+  let y = document.querySelector(".no-pointer");
+  y.classList.remove("grid");
+  // let z = document.querySelector("#dropdown-div");
+  // console.log(z.classList.contains("dropdown-hover"));
+    x.classList.add("dropdown-content");
+    }
+  // let y = document.querySelector(".no-pointer");
+  // y.classList.remove("grid");
+
+}
